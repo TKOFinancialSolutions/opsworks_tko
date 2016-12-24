67e9282e79abb0aea3b6e77f83ee032a8d700591
Kevin Pheasey <kevin@kpheasey.com>
2016-12-24 13:26:47 -0500

    Use code attributes on bash blocks

--- recipes/phantomjs.rb
+++ recipes/phantomjs.rb
@@ -6,8 +6,22 @@
 
 version = node['phantomjs']['version']
 
-bash "wget https://bitbucket.org/ariya/phantomjs/downloads/#{version}.tar.bz2"
-bash "sudo tar xvjf #{version}.tar.bz2"
-bash "mv #{version} /usr/local/share"
-bash "ln -sf /usr/local/share/#{version}/bin/phantomjs /usr/local/bin"
-bash "rm #{version}.tar.bz2"
+bash 'download' do
+  code "wget https://bitbucket.org/ariya/phantomjs/downloads/#{version}.tar.bz2"
+end
+
+bash 'extract' do
+  "sudo tar xvjf #{version}.tar.bz2"
+end
+
+bash 'move' do
+  code "mv #{version} /usr/local/share"
+end
+
+bash 'link' do
+  code "ln -sf /usr/local/share/#{version}/bin/phantomjs /usr/local/bin"
+end
+
+bash 'cleanup' do
+  code "rm #{version}.tar.bz2"
+end
